8954f1800721a2a61d5e24893c264f54249c06d1
tk4vr <78142997+tk4vr@users.noreply.github.com>
2021-03-31 18:38:05 +0300

    second commit

--- app/src/main/java/com/example/oop_project/MainActivity.java
+++ app/src/main/java/com/example/oop_project/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.oop_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent= new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
