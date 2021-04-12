529e2c14e6c2e6d672e07dd9d84d150db96570de
Anirudh-Sundar <43537202+Anirudh-Sundar@users.noreply.github.com>
2021-04-12 14:43:51 +0530

    by Ani

--- app/src/main/java/com/example/oop_project/Customer/Shopadapter.java
+++ app/src/main/java/com/example/oop_project/Customer/Shopadapter.java
@@ -1,25 +1,39 @@
 package com.example.oop_project.Customer;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.oop_project.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseError;
 
 public class Shopadapter extends FirebaseRecyclerAdapter<model_shop, Shopadapter.myviewholder> {
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
 
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+    double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return (dist/1000);
 
+    }
     public Shopadapter(@NonNull FirebaseRecyclerOptions<model_shop> options) {
         super(options);
     }
@@ -37,13 +51,15 @@ public class Shopadapter extends FirebaseRecyclerAdapter<model_shop, Shopadapter
 
     @Override
     protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull final model_shop model) {
-
+        double lat1,lon1,lat2=0,lon2 = 0;//assigned as 0 till abdul finished sharedPref
+        lat1 =  model.getLatitude();
+        lon1 = model.getLongitude();
+        double dist = distance(lat1,lon1,lat2,lon2);
         holder.rate.setText(model.getPrice());
-        holder.qty.setText(model.getQuantity());
+        holder.qty.setText( model.getQuantity());
         holder.shopname.setText(model.getRname());
-
-
-        holder.tc.setText("total cost");
+        holder.dist.setText(""+dist);
+        holder.tc.setText("" + model.getPrice());
 
 
 
@@ -67,16 +83,15 @@ public class Shopadapter extends FirebaseRecyclerAdapter<model_shop, Shopadapter
 
     public class myviewholder extends RecyclerView.ViewHolder {
         //modify this based on what ani designs
-        TextView shopname,qty,rate,tc;
+        TextView shopname,qty,rate,tc,dist,lon;
 
         public myviewholder(@NonNull View itemView) {
             super(itemView);
-
             shopname = itemView.findViewById(R.id.shopnameTV);
             qty = itemView.findViewById(R.id.qtyTV);
             rate = itemView.findViewById(R.id.rateTV);
             tc = itemView.findViewById(R.id.totalcostTV);
-
+            dist = itemView.findViewById(R.id.distTV);
 
         }
     }
