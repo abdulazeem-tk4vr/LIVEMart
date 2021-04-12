598ac3c138b04aaf52e3ebb883e5dbe92efaf764
tk4vr <78142997+tk4vr@users.noreply.github.com>
2021-04-12 13:33:09 +0300

    datatype

--- app/src/main/java/com/example/oop_project/Customer/model_shop.java
+++ app/src/main/java/com/example/oop_project/Customer/model_shop.java
@@ -1,15 +1,15 @@
 package com.example.oop_project.Customer;
 
 public class model_shop {
-    double latitude, longitude ;
-    Integer quantity,price;
+    String latitude, longitude ;
+    String quantity,price;
     String rname;
 
     public model_shop() {
     }
 
 
-    public model_shop(double latitude, double longitude, int price, Integer quantity, String rname) {
+    public model_shop(String latitude, String longitude, String price, String quantity, String rname) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.price = price;
@@ -17,35 +17,35 @@ public class model_shop {
         this.rname = rname;
     }
 
-    public double getLatitude() {
+    public String getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
+    public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
 
-    public double getLongitude() {
+    public String getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
+    public void setLongitude(String longitude) {
         this.longitude = longitude;
     }
 
-    public int getPrice() {
+    public String getPrice() {
         return price;
     }
 
-    public void setPrice(Integer price) {
+    public void setPrice(String price) {
         this.price = price;
     }
 
-    public Integer getQuantity() {
+    public String getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(Integer quantity) {
+    public void setQuantity(String quantity) {
         this.quantity = quantity;
     }
 
