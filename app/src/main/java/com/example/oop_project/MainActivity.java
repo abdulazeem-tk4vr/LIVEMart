4977a046ab42c238f34671788e7e70a3b8f72946
tk4vr <78142997+tk4vr@users.noreply.github.com>
2021-04-15 09:35:03 +0300

    mkm

--- app/src/main/java/com/example/oop_project/MainActivity.java
+++ app/src/main/java/com/example/oop_project/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         navbartestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, addproduct.class);
+                Intent intent = new Intent(MainActivity.this, NavigationBar.class);
                 startActivity(intent);
 
 
