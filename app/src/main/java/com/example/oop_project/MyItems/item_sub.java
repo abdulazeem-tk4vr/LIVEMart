084d01aa87cc662c380be83e8a3454b694810bde
tk4vr <78142997+tk4vr@users.noreply.github.com>
2021-04-19 22:09:12 +0300

    hello

--- app/src/main/java/com/example/oop_project/MyItems/item_sub.java
+++ app/src/main/java/com/example/oop_project/MyItems/item_sub.java
@@ -3,64 +3,40 @@ package com.example.oop_project.MyItems;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.oop_project.R;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link item_sub#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class item_sub extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public item_sub() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment item_sub.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static item_sub newInstance(String param1, String param2) {
-        item_sub fragment = new item_sub();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView recview;
+    adapter_itemsub adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_sub, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_item_sub, container, false);
+
+        recview = (RecyclerView) view.findViewById(R.id.recview_itemssub);
+        recview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+//        FirebaseRecyclerOptions<model_items_cat> options =
+//                new FirebaseRecyclerOptions.Builder<model_items_cat>()
+//                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Products").child("Categories").child(), model_itemsub.class)
+//                        .build();
+//
+//        adapter = new adapter_itemsub(options);
+//        recview.setAdapter(adapter);
+
+
+        return  view;
     }
 }
