e1f49a48fe1f8e21517b94360385e6d0f07c6453
tk4vr <78142997+tk4vr@users.noreply.github.com>
2021-04-23 22:38:49 +0300

    proc

---
+++ app/src/main/java/com/example/oop_project/Main/Admin/Admin.java
@@ -0,0 +1,66 @@
+package com.example.oop_project.Main.Admin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.oop_project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Admin#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Admin extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Admin() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Admin.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Admin newInstance(String param1, String param2) {
+        Admin fragment = new Admin();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin, container, false);
+    }
+}
