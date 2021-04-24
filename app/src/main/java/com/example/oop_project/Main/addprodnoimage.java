2c7d02e3b42280fcbc6e45e47f828e5a2f3fcc37
Karthik Suresh <karthiksuresh324@gmail.com>
2021-04-24 18:18:51 +0530

    retailer stuffs

--- app/src/main/java/com/example/oop_project/Main/addprodnoimage.java
+++ app/src/main/java/com/example/oop_project/Main/addprodnoimage.java
@@ -1,175 +1,175 @@
-//package com.example.oop_project.Main;
-//
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.content.SharedPreferences;
-//import android.net.Uri;
-//import android.os.Bundle;
-//import android.service.autofill.UserData;
-//import android.text.TextUtils;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.ImageView;
-//import android.widget.Spinner;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import com.example.oop_project.R;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.google.firebase.storage.FirebaseStorage;
-//import com.google.firebase.storage.StorageReference;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//
-//public class addprodnoimage extends AppCompatActivity  {
-//
-//    private String quantity, Price, Pname, p_username,nkey,category;
-//    double temp_quantity = 0;
-//    private ImageView InputProductImage;
-//    private static final int GalleryPick = 1;
-//    private Uri ImageUri;
-//    private Button AddNewProductButton;
-//    private TextView pname, pquantity, pprice;
-//
-//    private DatabaseReference ProductsRef,UserData,Quantdata;
-//    private Spinner product_type;
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.addnoimage);
-//
-//
-//        {
-//            SharedPreferences sh = getApplicationContext().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
-//
-//            // The value will be default as empty string because for
-//// the very first time when the app is opened, there is nothing to show
-//            p_username = sh.getString("username", "Babuwhole");
-//            Log.i("memes",p_username);
-//// We can then use the data
-//        }//SharedPrefs
-//
-//        InputProductImage = findViewById(R.id.select_product_image);
-//        AddNewProductButton = findViewById(R.id.add_new_product);
-//        pname = findViewById(R.id.product_name);
-//        pprice = findViewById(R.id.product_price);
-//        pquantity = findViewById(R.id.product_quantity);
-//        ProductsRef = FirebaseDatabase.getInstance().getReference().child("User");
-//
-//
-//        {
-//            product_type = (Spinner) findViewById(R.id.spinner);
-//            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
-//                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Products));
-//            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//            product_type.setAdapter(myAdapter);
-//        } //Spinner Product Type Initialization
-//
-//
-//        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ValidateProductData();
-//            }
-//        });
-//
-//
-//    }
-//
-//    private void OpenGallery() {
-//
-//        Intent galleryIntent = new Intent();
-//        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-//        galleryIntent.setType("image/*");
-//        startActivityForResult(galleryIntent, GalleryPick);
-//    }
-//
-//    private void ValidateProductData() {
-//        Price = pprice.getText().toString().trim();
-//        Pname = pname.getText().toString().trim();
-//        quantity = pquantity.getText().toString().trim();
-//        category = product_type.getSelectedItem().toString();
-//
-//        if (TextUtils.isEmpty(quantity)) {
-//            Toast.makeText(this, "Please write product quantity...", Toast.LENGTH_SHORT).show();
-//        } else
-//        if (TextUtils.isEmpty(Price)) {
-//            Toast.makeText(this, "Please write product Price...", Toast.LENGTH_SHORT).show();
-//        }
-//        if (TextUtils.isEmpty(Pname)) {
-//            Toast.makeText(this, "Please write product name...", Toast.LENGTH_SHORT).show();
-//        } else {
-//            SaveProductInfoToDatabase();
-//        }
-//    }
-//
-//
-//
-//    private void SaveProductInfoToDatabase() {
-//        Log.i("username",p_username +" 1"+  category +" 2" + Pname);
-//        UserData = FirebaseDatabase.getInstance().getReference().child("User").child("Wholesaler").child(p_username).child("Products").child(category).child(Pname);
-//        UserData.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                if(snapshot.child("quantity").exists()) {
-//                    temp_quantity = Double.parseDouble(snapshot.child("quantity").getValue().toString().trim());
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
-//
-//
-//
-//
-//
-//        Double qty = Double.parseDouble(quantity) + temp_quantity;
-//        HashMap<String, Object> productMap = new HashMap<>();
-//        productMap.put("quantity", String.valueOf(qty));
-//        //Log.i("qualpran","check"+quantity);
-//        productMap.put("price",Price);
-//        productMap.put("pname",pname);
-//
-////        FirebaseDatabase.getInstance().getReference().child("User").child("Wholesaler").
-////                child(p_username).child("Products").child(category).child(Pname).updateChildren(productMap);
-//
-//        // if snapshot exists
-//        // Quantity = Quantity + temp_quantity;
-//        // update
-////        if(InputProduct)
-//
-//       Quantdata.child(category).child(Pname).child("Wholesaler").child(p_username).updateChildren(productMap);
-//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//        startActivity(intent);
-//    }
-//
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
-//            ImageUri = data.getData();
-//            InputProductImage.setImageURI(ImageUri);
-//        }
-//    }
-//}
+package com.example.oop_project.Main;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.service.autofill.UserData;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.oop_project.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class addprodnoimage extends AppCompatActivity  {
+
+    private String quantity, Price, Pname, p_username,nkey,category;
+    double temp_quantity = 0;
+    private ImageView InputProductImage;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+    private Button AddNewProductButton;
+    private TextView pname, pquantity, pprice;
+
+    private DatabaseReference ProductsRef,UserData,Quantdata;
+    private Spinner product_type;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addnoimage);
+
+
+        {
+            SharedPreferences sh = getApplicationContext().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+
+            // The value will be default as empty string because for
+// the very first time when the app is opened, there is nothing to show
+            p_username = sh.getString("username", "Babuwhole");
+            Log.i("memes",p_username);
+// We can then use the data
+        }//SharedPrefs
+
+        InputProductImage = findViewById(R.id.select_product_image);
+        AddNewProductButton = findViewById(R.id.add_new_product);
+        pname = findViewById(R.id.product_name);
+        pprice = findViewById(R.id.product_price);
+        pquantity = findViewById(R.id.product_quantity);
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("User");
+
+
+        {
+            product_type = (Spinner) findViewById(R.id.spinner);
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Products));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            product_type.setAdapter(myAdapter);
+        } //Spinner Product Type Initialization
+
+
+        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValidateProductData();
+            }
+        });
+
+
+    }
+
+    private void OpenGallery() {
+
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+    private void ValidateProductData() {
+        Price = pprice.getText().toString().trim();
+        Pname = pname.getText().toString().trim();
+        quantity = pquantity.getText().toString().trim();
+        category = product_type.getSelectedItem().toString();
+
+        if (TextUtils.isEmpty(quantity)) {
+            Toast.makeText(this, "Please write product quantity...", Toast.LENGTH_SHORT).show();
+        } else
+        if (TextUtils.isEmpty(Price)) {
+            Toast.makeText(this, "Please write product Price...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(Pname)) {
+            Toast.makeText(this, "Please write product name...", Toast.LENGTH_SHORT).show();
+        } else {
+            SaveProductInfoToDatabase();
+        }
+    }
+
+
+
+    private void SaveProductInfoToDatabase() {
+        Log.i("username",p_username +" 1"+  category +" 2" + Pname);
+        UserData = FirebaseDatabase.getInstance().getReference().child("User").child("Wholesaler").child(p_username).child("Products").child(category).child(Pname);
+        UserData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.child("quantity").exists()) {
+                    temp_quantity = Double.parseDouble(snapshot.child("quantity").getValue().toString().trim());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+        Double qty = Double.parseDouble(quantity) + temp_quantity;
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("quantity", String.valueOf(qty));
+        //Log.i("qualpran","check"+quantity);
+        productMap.put("price",Price);
+        productMap.put("pname",pname);
+
+//        FirebaseDatabase.getInstance().getReference().child("User").child("Wholesaler").
+//                child(p_username).child("Products").child(category).child(Pname).updateChildren(productMap);
+
+        // if snapshot exists
+        // Quantity = Quantity + temp_quantity;
+        // update
+//        if(InputProduct)
+
+       Quantdata.child(category).child(Pname).child("Wholesaler").child(p_username).updateChildren(productMap);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            ImageUri = data.getData();
+            InputProductImage.setImageURI(ImageUri);
+        }
+    }
+}
